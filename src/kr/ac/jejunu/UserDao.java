e0621da3a4af31032b9ac8c2ebb0495575cd7721
gunooh <gunooh12@gmail.com>
2017-03-15 21:38:28 +0900

    userDao
    userDao

---
+++ src/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,55 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+/**
+ * Created by hyh0408 on 2017. 3. 15..
+ */
+public class UserDao {
+    public User get(Long id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+
+    public Long add(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUES (?,?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        Long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+//        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "root", "jejupw");
+        return DriverManager.getConnection("jdbc:mysql://localhost/", "root", "w3jufg)JuU5I");
+    }
+}
---
+++ test/kr/ac/jejunu/UserDaoTest.java
@@ -0,0 +1,41 @@
+package kr.ac.jejunu;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+/**
+ * Created by hyh0408 on 2017. 3. 15..
+ */
+public class UserDaoTest {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        Long id = 1L;
+        String name = "허윤호";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+        assertThat(id, is(user.getId()));
+        assertThat(name, is(user.getName()));
+        assertThat(password, is(user.getPassword()));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        String name = "헐크";
+        String password = "1111";
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        Long id = userDao.add(user);
+        User resultUser = userDao.get(id);
+        assertThat(id, is(resultUser.getId()));
+        assertThat(name, is(resultUser.getName()));
+        assertThat(password, is(resultUser.getPassword()));
+
+    }
+}
